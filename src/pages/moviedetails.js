71dccfa4e4f82599b4ce9d41a9cf03ced0d96a47
peterige2004 <51337786+peterige2004@users.noreply.github.com>
2023-09-17 13:35:50 +0100

    moviedetails.js

--- src/pages/moviedetails.js
+++ src/pages/moviedetails.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react'
+import "./movie.css"
 
-function MovieDetails(){
+function Moviepage(props) {
+ return(
+    <div>
+        <div className='movie-head'>
+            <img src='./Images/tv.png'></img>
+            <p>MovieBox</p>
+        </div>
 
+        <img className='movie-img' src={`https://image.tmdb.org/t/p/original${props.items .backdrop_path}`}></img>
+
+        <p data-testid="movie-title" className='title'>{props.items.original_title}</p>
+        <p data-testid="movie-release-date" id='movie-release-date'></p>
+        <p data-testid="movie-runtime"></p>
+        <p data-testid="movie-overview" className='overview'>{props.items.overview}</p>
+    </div>
+  )
 }
 
-export default MovieDetails
+export default Moviepage
