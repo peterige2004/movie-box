5520ec5ed466a80b0f0a5813c149a847751e3725
peterige2004 <51337786+peterige2004@users.noreply.github.com>
2023-09-17 13:36:58 +0100

    Footer.js

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,14 +1,23 @@
-import React from "react";
-import './footer.css'
+import React from 'react'
 
-function Footer(){
-    return(
-        <div className="footer">
-          <h2>condition use</h2>
-          <h2>Privacy & Policy</h2>
-          <h2>Press Room</h2>
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className='footer-img'>
+            <img src='../Images/fa-brands_facebook-square.png'></img>
+            <img src='../Images/fa-brands_youtube.png'></img>
+            <img src='../Images/fa-brands_twitter.png'></img>
+            <img src='../Images/fa-brands_instagram.png'></img>
         </div>
-    )
+        <div className='footer-text'>
+            <p>Conditions of Use</p>
+            <p>Privacy & Policy</p>
+            <p>Press Room</p>
+        </div>
+
+        <p className='footer-info'>© 2023 MovieBox by Tella Oyinkansola</p>
+    </div>
+  )
 }
 
-export default Footer;
+export default Footer
